978b95c8e06dfa9bbaf76032ed7483988f3b7ac1
Yvonne Mogere <108934423+VonneySnow@users.noreply.github.com>
2024-03-23 00:09:08 +0300

    Add files via upload

---
+++ dart week 2 assignment.dart
@@ -0,0 +1,19 @@
+void main() {
+  //numbers which carry numerical values and can either be integer ( 10,20,1) or double(1.5, 1/3)
+  int height = 150;
+  double weight = 5.9;
+  print(height);
+  // String which store text. the text must be stored in quots '' or ""
+  String name = 'Yvonne';
+  print(name);
+  // boolean which carries true or false functions
+  bool is_married = false;
+  print(is_married);
+//List which holds ordered collection of values
+  List names = ['Dante', 'Liam', 'Dorothy', 'Godfrey', 'Natalie'];
+  print(names);
+  // values in a list can be accesed through indexes as bellow
+  print(names[0]);
+  // Map is a collection that represent sets of value pairs.
+  Map ages = {'john': 26, 'alice': 30, 'natasha': 17};
+}
